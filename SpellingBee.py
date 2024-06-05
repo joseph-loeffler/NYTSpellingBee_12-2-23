f79a4270bb2136b04633949d398cb0deb7273aba
Joseph Loeffler <joseph.loeffler@duke.edu>
2024-06-05 14:54:17 -0700

    no real changes

--- SpellingBee.py
+++ SpellingBee.py
@@ -1,16 +1,19 @@
 yellow_letter = set(['c'])
-letters = set(['g', 'h', 'a', 'i', 'm', 'n', 'c'])
+letters = set(['h', 't', 'e', 'c', 'm', 'a', 'n'])
 
 file = open("dictionary.txt")
 dict = file.readlines()
 
 answerList = []
+anagramList = []
 for word in dict:
     word = word.strip()
     if len(word) > 3:
         w = set(word)
         if (len(w & yellow_letter) == 1) and (len(w - letters) == 0):
             answerList.append(word)
+            if w == letters:
+                anagramList.append(word)
 
 print(answerList)
-print(len(answerList))
+print(anagramList)
